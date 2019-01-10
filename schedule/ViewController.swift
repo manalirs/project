51f226b38261a1e07665264ef798cae0b71af173
Yuj-admin-mac02 <yuj-admin-mac02@apples-macbook-pro.local>
2019-01-10 17:32:44 +0530

    Initial Commit

---
+++ schedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  schedule
+//
+//  Created by Yuj-admin-mac02 on 10/01/19.
+//  Copyright © 2019 Yuj-admin-mac02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
